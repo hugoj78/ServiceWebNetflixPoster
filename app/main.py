f313c81d69541c7253e4c495c4a717a6363e26c7
hugoj78 <hugoj.egu@gmail.com>
2022-03-26 22:34:30 +0100

    add cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes.posters import router as poster
 from src.routes.health import router as health
 from config.openapi import tags_metadata
 import os
 
 app = FastAPI(
-    title="Web Services Poster",
+    title="WebServiceNetflix - Poster",
     description="a REST API using python and mysql",
     version="0.0.1",
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(poster)
